aa57d7c70831f48a54bb6a7dd5269737429994e1
rafaelim <rafael.lima@univem.edu.br>
2018-07-16 20:16:36 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const pipeline = (...funcs) => (val) => funcs.reduce((prev, func) => func(prev), val);
+
+capitalize = it => it[0].toUpperCase() + it.slice(1);
+exclaim = it => it + "!";
+double = it => it + it;
+new_line = it => it + "\n";
+
+const x = pipeline(
+  capitalize,
+  exclaim,
+  new_line,
+  double
+)("hello world")
+
+console.log(x)
